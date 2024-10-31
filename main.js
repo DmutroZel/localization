7e187cd44056939cc9df9aadd1f8556751b7d0aa
DmutroZel <156704336+DmutroZel@users.noreply.github.com>
2024-10-31 15:22:00 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,160 @@
+let semanticCore = {
+    logo: {
+        "ua": "Лого",
+        "en": "Logo",
+        "pl": "Logo",
+        "jp": "ロゴ",
+        "fr": "Logo",
+        "lt": "Logotipas",
+        "pt": "Logótipo",
+        "es": "Logotipo",
+        "hu": "Logó",
+        "zh": "标志",
+        "ar": "الشعار",
+        "nl": "Logo"
+    },
+    home: {
+        "ua": "Домашня",
+        "en": "Home",
+        "pl": "Strona główna",
+        "jp": "ホーム",
+        "fr": "Accueil",
+        "lt": "Pagrindinis",
+        "pt": "Início",
+        "es": "Inicio",
+        "hu": "Főoldal",
+        "zh": "主页",
+        "ar": "الصفحة الرئيسية",
+        "nl": "Thuis"
+    },
+    about: {
+        "ua": "Про нас",
+        "en": "About",
+        "pl": "O nas",
+        "jp": "私たちについて",
+        "fr": "À propos",
+        "lt": "Apie mus",
+        "pt": "Sobre",
+        "es": "Acerca de",
+        "hu": "Rólunk",
+        "zh": "关于我们",
+        "ar": "معلومات عنا",
+        "nl": "Over ons"
+    },
+    contact: {
+        "ua": "Контакти",
+        "en": "Contact",
+        "pl": "Kontakt",
+        "jp": "お問い合わせ",
+        "fr": "Contact",
+        "lt": "Kontaktai",
+        "pt": "Contacto",
+        "es": "Contacto",
+        "hu": "Kapcsolat",
+        "zh": "联系我们",
+        "ar": "اتصل بنا",
+        "nl": "Contact"
+    },
+    description: {
+        "ua": "Шукаєте когось, хто б правильно, коротко та якісно переклав ваш матеріал? Вам пощастило опинитися тут! Ми можемо перекласти все на понад 20 мов! Наші люди дуже освічені та розумні. Наша команда складається з носіїв мови та лінгвістів-поліглотів, які забезпечать вам надійний та зрозумілий переклад. Наша сучасна команда забезпечить вам розвиток і просування будь-якого бізнесу або матеріалу.",
+        "en": "Are you looking for someone to accurately, briefly, and qualitatively translate your material? You're in the right place! We can translate into more than 20 languages! Our people are very educated and smart. Our team consists of native speakers and linguist-polyglots who will provide you with a reliable and understandable translation. Our modern team will ensure the development and promotion of any business or material.",
+        "pl": "Szukasz kogoś, kto poprawnie, krótko i jakościowo przetłumaczy twój materiał? Masz szczęście być tutaj! Możemy przetłumaczyć wszystko na ponad 20 języków! Nasi ludzie są bardzo wykształceni i mądrzy. Nasz zespół składa się z native speakerów i poliglotów-lingwistów, którzy zapewnią ci wiarygodne i zrozumiałe tłumaczenie. Nasz nowoczesny zespół zapewni rozwój i promocję każdego biznesu lub materiału.",
+        "jp": "あなたの資料を正確に、簡潔に、質の高い翻訳を求めていませんか？ ここで運良く見つかりました！ 20を超える言語に翻訳することができます！ 私たちのメンバーは非常に教育を受けており、賢明です。 私たちのチームは、ネイティブスピーカーと、信頼できる理解しやすい翻訳を提供する多言語話者の言語学者で構成されています。 私たちの現代的なチームは、あらゆるビジネスや資料の発展と普及を確実にします。",
+        "fr": "Vous cherchez quelqu'un pour traduire votre matériel correctement, brièvement et de manière qualitative ? Vous avez de la chance de vous trouver ici ! Nous pouvons traduire dans plus de 20 langues ! Nos collaborateurs sont très instruits et intelligents. Notre équipe est composée de locuteurs natifs et de linguistes polyglottes, qui vous garantiront une traduction fiable et compréhensible. Notre équipe moderne assurera le développement et la promotion de toute entreprise ou matériel.",
+        "lt": "Ieškote kažko, kas teisingai, trumpai ir kokybiškai išverstų jūsų medžiagą? Jums pasisekė čia būti! Mes galime išversti į daugiau nei 20 kalbų! Mūsų žmonės yra labai išsilavinę ir protingi. Mūsų komanda susideda iš gimtąja kalba kalbančių žmonių ir lingvistų-poliglotų, kurie užtikrins jums patikimą ir suprantamą vertimą. Mūsų šiuolaikinė komanda užtikrins bet kurio verslo ar medžiagos plėtrą ir sklaidą.",
+        "pt": "Procurando alguém que traduza seu material corretamente, de forma breve e com qualidade? Você teve sorte de estar aqui! Podemos traduzir para mais de 20 idiomas! Nossa equipe é composta por pessoas altamente educadas e inteligentes. Nossa equipe é composta por falantes nativos e linguistas-poliglotas, que fornecerão uma tradução confiável e compreensível. Nossa equipe moderna garantirá o desenvolvimento e a promoção de qualquer negócio ou material.",
+        "es": "¿Buscas a alguien que traduzca tu material correctamente, de forma breve y cualitativa? ¡Tuviste suerte de estar aquí! ¡Podemos traducir a más de 20 idiomas! Nuestra gente es muy educada e inteligente. Nuestro equipo está formado por hablantes nativos y lingüistas políglotas, que te proporcionarán una traducción fiable y comprensible. Nuestro equipo moderno asegurará el desarrollo y la promoción de cualquier negocio o material.",
+        "hu": "Olyasvalakit keres, aki megfelelően, röviden és minőségileg fordítja le anyagát? Szerencséje van, hogy itt van! Több mint 20 nyelvre fordítunk! Munkatársaink nagyon képzettek és okosak. Csapatunk anyanyelvűekből és nyelvtudós poliglottokból áll, akik megbízható és érthető fordítást biztosítanak. Modern csapatunk garantálja bármely üzleti vagy anyagi projekt fejlesztését és népszerűsítését.",
+        "zh": "您在寻找能够准确、简洁、高质量地翻译您的材料的人吗？ 您来对地方了！ 我们可以翻译超过20种语言！ 我们的人非常有学识和聪明。 我们的团队由母语为母语的语言学家组成，他们将为您提供可靠且易于理解的翻译。 我们的现代团队将确保任何业务或材料的发展和推广。",
+        "ar": "هل تبحث عن شخص يترجم موادك بشكل صحيح ومختصر وبجودة عالية؟ لقد حالفك الحظ بالوصول إلى هنا! يمكننا الترجمة إلى أكثر من 20 لغة! فريقنا مؤهل للغاية وذكي. يتكون فريقنا من متحدثين أصليين ومتعددي اللغات الذين سيضمنون لك ترجمة موثوقة ومفهومة. سيضمن فريقنا الحديث تطوير وترويج أي عمل أو مادة.",
+        "nl": "Bent u op zoek naar iemand die uw materiaal correct, kort en kwalitatief vertaalt? U heeft geluk dat u hier bent! Wij kunnen vertalen in meer dan 20 talen! Onze mensen zijn zeer goed opgeleid en slim. Ons team bestaat uit moedertaalsprekers en linguïst-polygloten die u een betrouwbare en begrijpelijke vertaling zullen bieden. Ons moderne team zorgt voor de ontwikkeling en promotie van elk bedrijf of materiaal."
+    }
+};
+
+
+let allLang = ['en', 'ua', 'pl', 'jp', 'fr', 'lt', 'pt', 'es', 'hu', 'zh','ar', 'nl'];
+let lang = 'en';
+
+ua.onclick = function () {
+    lang = 'ua';
+    changeURL();
+}
+en.onclick = function () {
+    lang = 'en';
+    changeURL();
+}
+pl.onclick = function () {
+    lang = 'pl';
+    changeURL();
+}
+jp.onclick = function () {
+    lang = 'jp';
+    changeURL();
+}
+fr.onclick = function () {
+    lang = 'fr';
+    changeURL();
+}
+lt.onclick = function () {
+    lang = 'lt';
+    changeURL();
+}
+pt.onclick = function () {
+    lang = 'pt';
+    changeURL();
+}
+es.onclick = function () {
+    lang = 'es';
+    changeURL();
+}
+hu.onclick = function () {
+    lang = 'hu';
+    changeURL();
+}
+zh.onclick = function () {
+    lang = 'zh';
+    changeURL();
+}
+ar.onclick = function () {
+    lang = 'ar';
+    changeURL();
+}
+nl.onclick = function () {
+    lang = 'nl';
+    changeURL();
+}
+function changeURL() {
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+// changeURL();
+function changeLanguage() {
+    let hash = (window.location.hash).substring(1);
+    console.log(hash);
+
+    if (!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    for (let key in semanticCore) {
+        document.querySelector('.language-' + key).innerText = semanticCore[key][hash];
+        console.log(key);
+    }
+}
+changeLanguage();
+
+// Отримуємо поточну тему з localStorage або встановлюємо за замовчуванням 'light'
+let currentTheme = localStorage.getItem('theme') || 'light';
+const wrapElement = document.querySelector('.wrap');
+wrapElement.className = 'wrap ' + currentTheme + '-theme';
+
+document.getElementById('theme-toggle').onclick = function () {
+    // Перемикаємо між темною і світлою темою
+    currentTheme = (currentTheme === 'light') ? 'dark' : 'light';
+    
+    // Застосовуємо відповідний клас до елемента wrap
+    wrapElement.className = 'wrap ' + currentTheme + '-theme';
+    
+    // Зберігаємо вибрану тему в localStorage
+    localStorage.setItem('theme', currentTheme);
+};
